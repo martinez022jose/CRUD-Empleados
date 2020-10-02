1b2b384001ff9d3727acd35479acbb39aca1b859
Jose_Martinez <martinez022jose@gmail.com>
2020-10-01 22:00:39 -0300

    Se agrega interface de agregado mediante formulario

--- src/public/funcionalidad.js
+++ src/public/funcionalidad.js
@@ -1,6 +1,13 @@
 var resNo = document.getElementById('resNo');
 var pantalla = document.querySelectorAll('#total #up');
 var pantallaDos = document.getElementById('upp');
+var add = document.getElementById('add');
+var formulario = document.getElementById('form');
+
+
+add.addEventListener('click', () => {
+        formulario.style.display = "flex";
+});
 
 pantalla.forEach((elemento)=>{
     elemento.addEventListener('click', ()=>{
