97ca52647fb2473913ee2edc89ca9a8f80a651c6
Jose_Martinez <martinez022jose@gmail.com>
2020-10-01 22:40:10 -0300

    Se agrega funcionalidad correcta de boton delete

--- src/public/funcionalidad.js
+++ src/public/funcionalidad.js
@@ -1,8 +1,25 @@
 var resNo = document.getElementById('resNo');
 var pantalla = document.querySelectorAll('#total #up');
 var pantallaDos = document.getElementById('upp');
+
 var add = document.getElementById('add');
 var formulario = document.getElementById('form');
+
+var deles = document.querySelectorAll('#delete');
+var ventanaDelete = document.getElementById('ventanaDelete');
+var resNoDelete = document.getElementById('resNoDelete');
+
+
+deles.forEach((elemento) => {
+    elemento.addEventListener('click', () => {
+        ventanaDelete.classList.remove('ocultar');
+        
+    });
+});
+
+resNoDelete.addEventListener('click', () => {
+    ventanaDelete.classList.add('ocultar');
+});
 
 
 add.addEventListener('click', () => {
